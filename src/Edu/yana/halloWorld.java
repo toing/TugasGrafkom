8114c638fc18a508c0d93190e690c1535a0e9d9c
Yana <yana.toing@yahoo.co.id>
2012-05-17 12:46:51 +0700

    first commit

---
+++ src/Edu/yana/halloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Edu.yana;
+
+/**
+ *
+ * @author Toin9
+ */
+public class halloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hello World");
+    }
+
+}
